3617ed4243f08ab6242e80fae5d235111c475e15
ArgonGlow <koen2702@gmail.com>
2022-05-17 02:05:09 +0200

    added wind direction

--- app.js
+++ app.js
@@ -24,10 +24,32 @@ function getWeather(data) {
     // currentTemp.innerText = `Current temperature: ${data.current.temp} Celcius`
     // hourlyTemp.innerText = data.hourly[1].temp
 
+    let windDegrees = data.current.wind_deg
+    let windDirection = ''
+    console.log(windDirection)
+
+    if (windDegrees > 337.5 && windDegrees < 22.5) {
+        windDirection = 'N'
+    } else if (windDegrees > 292.5) {
+        windDirection = "NW"
+    } else if (windDegrees > 247.5) {
+        windDirection = 'W'
+    } else if (windDegrees > 202.5) {
+        windDirection = 'SW'
+    } else if (windDegrees > 157.5) {
+        windDirection = 'S'
+    } else if (windDegrees > 112.5) {
+        windDirection = 'SE'
+    } else if (windDegrees > 67.5) {
+        windDirection = 'E'
+    } else {
+        windDirection = 'NE'
+    }
+
     // weatherDiv.innerText = `Current weather: ${data.current.weather[0].main}/${data.current.weather[0].description}`
     weatherDiv.innerText = `${data.current.weather[0].main}`
     weatherIcon.src = `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`
-    currentTemp.innerText = `${data.current.temp.toString().slice(0,-1)} °C`
-    currentWind.innerText = `Wind
+    currentTemp.innerText = `${data.current.temp.toString().slice(0,-1)}°C`
+    currentWind.innerText = `${windDirection}, ${windDegrees}°
     ${data.current.wind_speed} m/s`
 }
